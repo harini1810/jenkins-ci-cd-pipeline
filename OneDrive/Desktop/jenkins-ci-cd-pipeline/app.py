6574e6c4ef79134c9218a70c1e1f14136d35891e
harini1810 <137413662+harini1810@users.noreply.github.com>
2025-02-10 19:55:11 +0530

    Update app.py

--- OneDrive/Desktop/jenkins-ci-cd-pipeline/app.py
+++ OneDrive/Desktop/jenkins-ci-cd-pipeline/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+omfrom flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Jenkins CI/CD!"
+    return "Hello, Jenkins CI/CD,Welcome!"
 
 if __name__ == "__main__":
     app.run(debug=True)
